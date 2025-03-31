d8b3094b3452ec6fd9787a03876a2234a9071f25
[Nishant Upadhyay] <nishantupadhyay813@gmail.com>
2025-03-31 15:08:09 +0530

    first commit

---
+++ client/app/Components/Header/Header.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { useTasks } from "@/context/taskContext";
+import { useUserContext } from "@/context/userContext";
+import { github, moon, profile } from "@/utils/Icons";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+
+function Header() {
+  const { user,logoutUser } = useUserContext();
+  const { openModalForAdd, activeTasks } = useTasks();
+
+  const router = useRouter();
+
+  const { name } = user;
+
+  const userId = user._id;
+
+  return (
+    <header className="px-6 my-4 w-full flex items-center justify-between bg-[#f9f9f9]">
+      <div>
+        <h1 className="text-lg font-medium">
+          <span role="img" aria-label="wave">
+            👋
+          </span>
+          {userId ? `Welcome, ${name}!` : "Welcome to Taskfyer"}
+        </h1>
+        <p className="text-sm">
+          {userId ? (
+            <>
+              You have{" "}
+              <span className="font-bold text-[#3aafae]">
+                {activeTasks.length}
+              </span>
+              &nbsp;active tasks
+            </>
+          ) : (
+            "Please login or register to view your tasks"
+          )}
+        </p>
+      </div>
+     {userId && <div className="h-[50px] flex items-center gap-[10.4rem]">
+        <button
+          className="px-8 py-3 bg-[#3aafae] text-white rounded-[50px]
+          hover:bg-[#00A1F1] hover:text-white transition-all duration-200 ease-in-out"
+          onClick={() => {
+            if (userId) {
+              openModalForAdd();
+            } else {
+              router.push("/login");
+            }
+          }}
+        >
+          { "Add a new Task"}
+        </button>
+        <button
+        className="mt-auto mb-6 mx-6 py-4 px-8 bg-[#EB4E31] text-white rounded-[50px] hover:bg-[#3aafae] transition duration-200 ease-in-out"
+        onClick={logoutUser}
+      >
+        Sign Out
+      </button>
+      </div>}
+    </header>
+  );
+}
+
+export default Header;
